e7f8ea42879bbec1ada6d29300d04857cbcc16ba
Iroyin Lozano <iroyin.salas@evertecinc.com>
2025-03-25 15:37:25 -0500

    se agrega la tabla de verbos

--- verbos/script.js
+++ verbos/script.js
@@ -1,5 +1,6 @@
 let verbs = []; 
 let currentVerb = {};
+let answered = false; // Flag para controlar el estado de la respuesta
 
 // Importa los verbos directamente desde el archivo verbs.js
 verbs = window.verbs;
@@ -12,20 +13,33 @@ function getRandomVerb() {
     document.getElementById('pastVerb').value = '';
     document.getElementById('participeVerb').value = '';
     document.getElementById('resultMessage').textContent = '';
+    document.getElementById('checkAnswer').textContent = 'Verificar';
+    answered = false; // Restablecer el estado de la respuesta
 }
 
 document.getElementById('checkAnswer').addEventListener('click', () => {
-    const base = document.getElementById('baseVerb').value.trim().toLowerCase();
-    const past = document.getElementById('pastVerb').value.trim().toLowerCase();
-    const participle = document.getElementById('participeVerb').value.trim().toLowerCase();
-    
-    if (base === currentVerb.baseVerb && past === currentVerb.passVerb && participle === currentVerb.participeVerb) {
-        document.getElementById('resultMessage').textContent = '¡Correcto!';
-        document.getElementById('resultMessage').className = 'correct';
+    if (!answered) {
+        const base = document.getElementById('baseVerb').value.trim().toLowerCase();
+        const past = document.getElementById('pastVerb').value.trim().toLowerCase();
+        const participle = document.getElementById('participeVerb').value.trim().toLowerCase();
+        
+        if (base === currentVerb.baseVerb && past === currentVerb.passVerb && participle === currentVerb.participeVerb) {
+            document.getElementById('resultMessage').textContent = '¡Correcto!';
+            document.getElementById('resultMessage').className = 'correct';
+        } else {
+            document.getElementById('resultMessage').textContent = `Incorrecto, la respuesta correcta es: ${currentVerb.baseVerb}, ${currentVerb.passVerb}, ${currentVerb.participeVerb}`;
+            document.getElementById('resultMessage').className = 'incorrect';
+        }
+        
+        document.getElementById('checkAnswer').textContent = 'Continuar';
+        answered = true;
     } else {
-        document.getElementById('resultMessage').textContent = `Incorrecto, la respuesta correcta es: ${currentVerb.baseVerb}, ${currentVerb.passVerb}, ${currentVerb.participeVerb}`;
-        document.getElementById('resultMessage').className = 'incorrect';
+        getRandomVerb();
     }
 });
 
-document.getElementById('nextVerb').addEventListener('click', getRandomVerb);
+function goToVerbsPage() {
+    window.location.href = 'verbs.html';
+}
+
+document.getElementById('viewVerbs').addEventListener('click', goToVerbsPage);
